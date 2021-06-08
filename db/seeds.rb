0cb5c2bbe9ed91bfac27cd82bd8a1800d877e95e
Gauderic Diagambana <diagambana.gauderic@gmail.com>
2021-06-08 16:56:25 +0200

    Articles created 10 times

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  articles = Article.new(title: Faker::Company.name, content: Faker::Twitter.status)
+  articles.save!
+end
